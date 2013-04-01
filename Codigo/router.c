026ca080fe52947e8226a4dad63a1162b87cc24e
Federico Rossi <federicomrossi@gmail.com>
2013-04-01 18:43:36 -0300

    Documentada la libreria router.h

--- Codigo/router.c
+++ Codigo/router.c
@@ -96,6 +96,7 @@ struct _route_t
  * FUNCIONES AUXILIARES
  * *****************************************************************/
 
+
 // Crea un host.
 // POST: devuelve un host o NULL si no ha sido posible llevar a cabo
 // la creación del mismo.
@@ -186,22 +187,6 @@ bool esPrefijo(const char *d, const char *q)
 
 	return true;
 }
-
-// Función que realiza la comparación alfanumérica de dos cadenas.
-// PRE: 'd' y 'q' son cadenas.
-// POST: devuelve < 0 si d<q, 0 si d==q, ó > 0 si d>q.
-// int comparacion_alfanumerica_cadenas(const char *d, const char *q)
-// {
-// 	int i, comp;
-// 	for(i = 0; d[i] && q[i]; i++)
-// 	{
-// 		comp = strcmp(d[i], q[i]);
-// 		if(comp < 0) return -1;
-// 		else if (comp > 0) return 1;
-// 	}
-
-// 	return 0;
-// }
 
 // Función que dada una línea de la sección [host] del archivo de 
 // especificación de ruteo, se encarga de parsear la información que en ella 
@@ -356,23 +341,18 @@ grafo_t* armar_red_archivo_de_entrada(char* archivo, lista_t *devices,
 		if(esPrefijo(buffer, S_HOST))
 		{
 			seccion = HOST;
-			printf("HOST\n");
 			continue;
 		}
 		else if(esPrefijo(buffer, S_DEVICE))
 		{
 			seccion = DEVICE;
-			printf("\nDEVICE\n");
 			continue;
 		}
 		else if(esPrefijo(buffer, S_ROUTE))
 		{
 			seccion = ROUTE;
-			printf("\nROUTE\n");
 			continue;
 		}
-
-		printf("linea: %s", buffer);
 
 		// Si no ha cambiado la sección, procesamos de acuerdo
 		// a la sección en la que nos encontremos
@@ -417,13 +397,49 @@ grafo_t* armar_red_entrada_estandar(lista_t *devices, lista_t *hosts)
 	return NULL;
 }
 
-// 
-void enviar_caminos_minimos_salida_estandar()
+// Función que se encarga de envíar a la salida estandar el camino
+// de datos desde un host origen hacia uno destino a través de los
+// dispositivos de la red.
+// PRE: 'origen' es el host origen desde el que se parte; 'destino' es
+// el host destino hasta el que se desea llegar; 'camino' es una lista
+// donde los elementos son los dispositivos que conforman el camino.
+// POST: Se envia a la salida estandar con una etiqueta de sección
+// definida como [route_path:NOMBRE_HOST_ORIGEN->NOMBRE_HOST_DESTINO]
+// y debajo de esta una serie de líneas que indican el dispositivo
+// por el que se debe pasar en cada paso del recorrido, cuyo formato es
+// [NUMERO_PASO:IP].
+void enviar_camino_salida_estandar(host_t* origen, host_t* destino,
+	lista_t *camino)
 {
+	int i = 1;
+
+	// Imprimimos sección
+	printf("[route_path:%s->%s]\n", origen->nombre, destino->nombre);
+	
+	// Imprimimos el host origen
+	printf("%d:%s\n", i, origen->ip);
+
+	lista_iter_t* iter = lista_iter_crear(camino);
+	lista_dato_t device;
+	
+	// Procesamos cada device del camino
+	while(!lista_iter_al_final(iter))
+	{
+		lista_iter_ver_actual(iter, &device);
+		
+		// Imprimimos el device
+		printf("%d:%s\n", ++i, ((device_t*)device)->ip);
+
+		lista_iter_avanzar(iter);
+	}
+	
+	lista_iter_destruir(iter);
+
+	// Imprimimos el host destino
+	printf("%d:%s\n", ++i, destino->ip);
+
 	return;
 }
-
-
 
 // Función que establece el criterio de selección de caminos para
 // el caso de poseer dos caminos de igual longitud.
@@ -431,8 +447,6 @@ void enviar_caminos_minimos_salida_estandar()
 // tipo 'device_t' (se los pasa como void por ser tratado por listas)
 int criterio_de_seleccion_de_camino(lista_dato_t c1, lista_dato_t c2)
 {
-	printf("c1: %s - ", ((device_t*) c1)->nombre);
-	printf("c2: %s\n", ((device_t*) c2)->nombre);	
 	return strcmp(((device_t*) c1)->nombre, ((device_t*) c2)->nombre);
 }
 
@@ -442,13 +456,38 @@ int criterio_de_seleccion_de_camino(lista_dato_t c1, lista_dato_t c2)
  * FUNCIONES DE LA LIBRERIA
  * *****************************************************************/
 
-// 
+
+// Función que procesa una red compuesta de hosts y dispositivos, tales
+// como routers, con el fin del encontrar el camino minimo para el envío
+// de datos desde un host origen hacia los demas hosts existentes en la
+// red.
+// PRE: 'archivo' es el nombre de archivo (inluyendo su extensión si es
+// que posee) en el que se encuentra la especificación de ruteo. 
+// El archivo debe estar dividido en secciones, con las siguientes 
+// etiquetas: '[host]', '[device]' y '[route]'. En la sección [host] se 
+// deben especificar los host que integran la red, con el formato 
+// '[NOMBRE],[IP],[NOMBRE_ROUTER]'. El primer host ingresado se considera
+// el host origen. En la sección [device] se deben especificar los
+// dispositivos que integran la red, con el formato '[NOMBRE_ROUTER],[IP]'.
+// El primer dispositivo ingresado se considera el dispositivo origen. 
+// Por último, la sección [route] contiene el registro de las conexiones 
+// entre los dispositivos, siendo su formato '[D1]->[D2],[PESO]', donde
+// D1 es el dispositivo de partida y D2 el dispositivo de llegada para 
+// ese tramo del recorrido.
+// Si no se especifica ningún archivo de entrada, el sistema solicitará
+// que se ingresen los datos a través de la entrada estandar, siendo
+// estrictamente necesario ingresarlos con el mismo formato establecido
+// anteriormente para los archivos.
+// POST: Los resultados se envían a la salida estandar.
 void procesar_red_caminos_minimos(char *archivo)
 {
+	// Variables y punteros utilizados
 	grafo_t *grafo_devices = NULL;
 	lista_t *devices = lista_crear();
 	lista_t *hosts = lista_crear();
-	lista_dato_t device_origen;
+	lista_t *camino;
+	lista_dato_t device_origen, host_origen, host_destino;
+
 
 	// Armamos el grafo de routers
 	if(archivo)
@@ -459,41 +498,44 @@ void procesar_red_caminos_minimos(char *archivo)
 		//grafo_routers = armar_red_entrada_estandar(devices, hosts);
 		printf("Procesamos hosts y devices desde entrada estandar");
 
-	// Buscamos caminos mínimos por Dijkstra
+	// En el primer elemento de la lista se encuentra el origen de los
+	// dispositivos
 	lista_ver_primero(devices, &device_origen);
+
+	// Procesamos el grafo y obtenemos los resultados de aplicar
+	// el algoritmo de Dijkstra
 	lista_t* resultados = dijkstra_caminos_minimos(grafo_devices,
 		device_origen, criterio_de_seleccion_de_camino);
 
-	lista_t *camino = dijkstra_obtener_camino(resultados, 
-		(lista_dato_t) buscar_device(devices, "5"));
 
+	// Creamos un iterador para la lista de hosts
+	lista_iter_t* iter = lista_iter_crear(hosts);
+	
+	// Tomamos el host origen
+	lista_iter_ver_actual(iter, &host_origen);
+	lista_iter_avanzar(iter);
 
-
-	//printf("largo: %d\n", lista_largo(camino));
-
-	// lista_dato_t aux;
-	// lista_ver_primero(camino, &aux);
-	// printf("nombre: %s\n", ((device_t*)aux)->nombre);
-
-	lista_iter_t* iter = lista_iter_crear(camino);
-	lista_dato_t aux;
-
+	// Procesamos los caminos hacia los demás hosts
 	while(!lista_iter_al_final(iter))
 	{
-		lista_iter_ver_actual(iter, &aux);
-		printf("nombre: %s\n", ((device_t*)aux)->nombre);
+		lista_iter_ver_actual(iter, &host_destino);
+		
+		// Solicitamos el camino hacia el destino
+		camino = dijkstra_obtener_camino(resultados, 
+			(lista_dato_t) buscar_device(devices, 
+				((host_t*)host_destino)->dispositivo_nombre));
+
+		// Enviamos a la salida estandar
+		enviar_camino_salida_estandar((host_t*) host_origen, 
+			(host_t*) host_destino, camino);
+
 		lista_iter_avanzar(iter);
 	}
 	
 	lista_iter_destruir(iter);
 
+	// Destruimos los resultados obtenidos por Dijkstra
 	dijkstra_destruir_resultados(resultados);
-
-	
-
-	/////printf("dispositivo: %s\n", ((device_t*)a)->nombre);
-
-	// Enviamos resultado a salida estandar
 
 	// Liberamos memoria utilizada
 	lista_destruir(devices, free);
--- Codigo/router.h
+++ Codigo/router.h
@@ -4,8 +4,9 @@
  * ******************************************************************
  * ******************************************************************
  * 
- * Librería de funciones aplicables a las redes y a los componentes
- * que en ellas se encuentran, tales como routers, hosts, etc.
+ * Librería de funciones aplicables a las redes y a los dispositivos
+ * y componentes que en ellas se encuentran, tales como routers, 
+ * hosts, switches, etc.
  */
 
 
@@ -18,7 +19,29 @@
  * FUNCIONES DE LA LIBRERIA
  * *****************************************************************/
 
-// [DOCUMENTAR]
+
+// Función que procesa una red compuesta de hosts y dispositivos, tales
+// como routers, con el fin del encontrar el camino minimo para el envío
+// de datos desde un host origen hacia los demas hosts existentes en la
+// red.
+// PRE: 'archivo' es el nombre de archivo (inluyendo su extensión si es
+// que posee) en el que se encuentra la especificación de ruteo. 
+// El archivo debe estar dividido en secciones, con las siguientes 
+// etiquetas: '[host]', '[device]' y '[route]'. En la sección [host] se 
+// deben especificar los host que integran la red, con el formato 
+// '[NOMBRE],[IP],[NOMBRE_ROUTER]'. El primer host ingresado se considera
+// el host origen. En la sección [device] se deben especificar los
+// dispositivos que integran la red, con el formato '[NOMBRE_ROUTER],[IP]'.
+// El primer dispositivo ingresado se considera el dispositivo origen. 
+// Por último, la sección [route] contiene el registro de las conexiones 
+// entre los dispositivos, siendo su formato '[D1]->[D2],[PESO]', donde
+// D1 es el dispositivo de partida y D2 el dispositivo de llegada para 
+// ese tramo del recorrido.
+// Si no se especifica ningún archivo de entrada, el sistema solicitará
+// que se ingresen los datos a través de la entrada estandar, siendo
+// estrictamente necesario ingresarlos con el mismo formato establecido
+// anteriormente para los archivos.
+// POST: Los resultados se envían a la salida estandar.
 void procesar_red_caminos_minimos(char *archivo);
 
 #endif
